a55dd4d71a0a096b747f35435d86de25529e9bcd
eiji.nakayasu <eiji.nakayasu@hogehoge.com>
2013-12-19 12:13:35 +0900

    Initial Commit

---
+++ GoogleNewsReaderTests/GoogleNewsReaderTests.m
@@ -0,0 +1,34 @@
+//
+//  GoogleNewsReaderTests.m
+//  GoogleNewsReaderTests
+//
+//  Created by 中安英治 on 2013/12/19.
+//  Copyright (c) 2013年 中安英治. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GoogleNewsReaderTests : XCTestCase
+
+@end
+
+@implementation GoogleNewsReaderTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
